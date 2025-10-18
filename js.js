ec4dea6fe0c800a29e4783d7ddea7474f83666fc
nasir21b@gmail.com <nasir21b@gmail.com>
2025-10-18 20:08:34 +0600

    video bg added

--- js.js
+++ js.js
@@ -1,18 +1,6 @@
+  
 
-let a = 5;
-
-
-function divide(x, y) {
-    if (y === 0) {
-        console.log("Error: Division by zero");
-        
-    }   
-    let result = x / y;
-    let message = `The division of ${x} by ${y} is ${result}`;
-    console.log(message);
-    return result;
-}   
-
-divide(a, 0);   
+const div = document.getElementById('test');
+console.log(div.dataset.name, div.dataset.price, div.dataset.color);
 
 
