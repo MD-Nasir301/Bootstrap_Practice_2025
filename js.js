407b4d016e77fb1236159335037df5672067eb8b
nasir21b@gmail.com <nasir21b@gmail.com>
2025-10-26 15:22:07 +0600

    CSS cross browers code set

--- js.js
+++ js.js
@@ -1,73 +1,17 @@
-  
-
-const div = document.getElementById('test');
-console.log(div.dataset.name, div.dataset.price, div.dataset.color);
 
 
-// 🪄 JS দিয়ে ব্যাকগ্রাউন্ড পজিশন স্ক্রল অনুযায়ী পরিবর্তন করা হচ্ছে
-window.addEventListener("scroll", function() {
-  const scrollY = window.scrollY;
-  const hero = document.querySelector(".hero");
-  hero.style.backgroundPositionY = `${scrollY * .5}px`;
-});
+  (() => {
+  'use strict';
 
+  const forms = document.querySelectorAll('.needs-validation');
 
-const title = document.getElementById("title");
-const trigger = 3100; // scroll position যখন fixed হবে
-
-window.addEventListener("scroll", () => {
-  const scroll = window.scrollY;
-
-  if (scroll > trigger) {
-    title.classList.add("fixed");
-    title.style.transform = "translateY(0)"; // smooth top move
-  } else {
-    title.classList.remove("fixed");
-    title.style.transform = "translateY(3200px)"; // scroll হওয়ার আগের offset
-  }
-});
-
-const box = document.querySelector('.box');
-const container = document.querySelector('.container'); // পেরেন্টে perspective থাকে
-
-container.addEventListener('mousemove', (e) => {
-  const rect = container.getBoundingClientRect();
-  const x = e.clientX - rect.left;  // container এর ভিতরে X অবস্থান
-  const y = e.clientY - rect.top;   // container এর ভিতরে Y অবস্থান
-
-  const midX = rect.width / 2;
-  const midY = rect.height / 2;
-
-  const rotateY = (x - midX) / 10;  // মাউস ডানে-বামে ঘোরানো
-  const rotateX = -(y - midY) / 10; // মাউস উপরে-নিচে ঘোরানো
-
-  box.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-});
-
-// মাউস চলে গেলে আবার আগের অবস্থায় ফিরে যাবে
-container.addEventListener('mouseleave', () => {
-  box.style.transform = 'rotateX(0deg) rotateY(0deg)';
-});
-
-let count = 0;
-
-let b = 5;
-let c = 1;
-
-
-function add(aNumber, bNumber) {
-  return aNumber + bNumber;
-}
-
-// Removed debugging statements and standardized variable names
-// Improved readability by using descriptive names and proper spacing
-
-let result = sum();
-console.log(result)
-
-
-  let fruits = ['apple', 'banana', false, NaN, 'mango'];
-
-  let filteredFruits = fruits.filter(Boolean);
-  console.log(filteredFruits); // Output: ['apple', 'banana', 'mango']
-  
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault(); // ফর্ম সাবমিট হতে দেবে না
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated'); // ভ্যালিডেশন দেখাবে
+    }, false);
+  });
+})();
