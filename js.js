20467cf5234972ba9277b266d723be9d43525c30
nasir21b@gmail.com <nasir21b@gmail.com>
2025-10-30 14:20:24 +0600

    Bootstrap Carouset Auto play added

--- js.js
+++ js.js
@@ -1,37 +1,43 @@
-(() => {
-  "use strict";
+// (() => {
+//   "use strict";
 
-  const forms = document.querySelectorAll(".needs-validation");
+//   const forms = document.querySelectorAll(".needs-validation");
 
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault(); // ফর্ম সাবমিট হতে দেবে না
-          event.stopPropagation();
-        }
-        form.classList.add("was-validated"); // ভ্যালিডেশন দেখাবে
-      },
-      false
-    );
-  });
-})();
+//   Array.from(forms).forEach((form) => {
+//     form.addEventListener(
+//       "submit",
+//       (event) => {
+//         if (!form.checkValidity()) {
+//           event.preventDefault(); // ফর্ম সাবমিট হতে দেবে না
+//           event.stopPropagation();
+//         }
+//         form.classList.add("was-validated"); // ভ্যালিডেশন দেখাবে
+//       },
+//       false
+//     );
+//   });
+// })();
 
-let aler = document.querySelector(".aler");
-let alertContent = document.querySelector(".alert-content");
-let close = document.querySelector(".close");
-aler.addEventListener("click", () => {
-  alertContent.classList.remove("d-none");
+// let aler = document.querySelector(".aler");
+// let alertContent = document.querySelector(".alert-content");
+// let close = document.querySelector(".close");
+// aler.addEventListener("click", () => {
+//   alertContent.classList.remove("d-none");
   
 
-});
+// });
 
-close.addEventListener("click", () => {
-  alertContent.classList.remove("d-block");
-  alertContent.classList.add("d-none");
-});
+// close.addEventListener("click", () => {
+//   alertContent.classList.remove("d-block");
+//   alertContent.classList.add("d-none");
+// });
 
-const alertList = document.querySelectorAll('.alert')
-const alerts = [...alertList].map(element => new bootstrap.Alert(element))
+// const alertList = document.querySelectorAll('.alert')
+// const alerts = [...alertList].map(element => new bootstrap.Alert(element))
+
+
+
+
+const slides = document.querySelectorAll('.slider-area .single-slider');
+console.log('মোট স্লাইড:', slides.length);
 
